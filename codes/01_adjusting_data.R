a63c24d5aded011b246adc42c935de9a2ba0209b
Luan Rezende Eduardo <luan.eduardo@ufrgs.br>
2019-11-29 11:22:04 -0300

    corrected

---
+++ codes/01_adjusting_data.R
@@ -0,0 +1,25 @@
+#packages
+library(readxl)
+library(xts)
+library(zoo)
+library(PerformanceAnalytics)
+
+#loading data into a dataframe
+closing_prices <- readxl::read_xlsx('data/acoesBR.xlsx', na = "-", )
+
+#creating the index
+date_string <- as.Date(closing_prices$Data)
+
+#creating the time series
+petr4_xts <- xts::xts(closing_prices[, 393], order.by = date_string)
+colnames(petr4_xts) <- "PETR4"
+
+#excluding NAs
+petr4_xts <- na.omit(petr4_xts)
+
+#calculating log returns
+petr4_returns <- PerformanceAnalytics::Return.calculate(petr4_xts, method = "log")
+petr4_returns <- na.omit(petr4_returns)
+
+saveRDS(petr4_xts, file = "data/petr4.rds")
+saveRDS(petr4_returns, file = "data/p4_returns.rds")
