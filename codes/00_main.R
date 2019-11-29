ad9df3dac52b82544a7b8213c10285cd30beefeb
Luan Rezende Eduardo <luan.eduardo@ufrgs.br>
2019-11-29 15:50:00 -0300

    under construction

---
+++ codes/00_main.R
@@ -0,0 +1,11 @@
+#This code uses ARIMA and GARCH models to make predictions on Petrobras
+#stocks returns (PETR4)
+
+#cleaning and adjusting the data
+source("codes/01_adjusting_data.R")
+
+#exploratory analysis
+source("codes/02_exploratory_analysis.R")
+
+#ARIMA modelling
+source("codes/03_arima_modelling")
